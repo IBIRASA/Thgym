1ee01e7c3ae9e2a2ae10c3d8e9e98e76eb4bd4a5
IBIRASA <d.ishimwe@alustudent.com>
2024-11-08 09:56:46 +0200

    updated

---
+++ Conditions/conditions.js
@@ -0,0 +1,25 @@
+
+/* the different between if else and else if is that ie else handle binary decisons(tru/false) while else-if handle multiple codntions more than two*/
+
+// var rain = "raining";
+// var sun ="shining";
+
+// if (rain == "raining") {
+//   console.log("It's raining outside!");
+// }
+// else{
+//   console.log("It's not raining outside!");
+// }
+
+
+var number1 = 20;
+
+if(number1%2 == 0)
+{
+  console.log("it's even")
+}
+else {
+  console.log("it's odd")
+}
+
+
